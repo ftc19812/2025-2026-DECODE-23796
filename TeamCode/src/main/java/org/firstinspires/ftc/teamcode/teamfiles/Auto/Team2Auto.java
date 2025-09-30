61bbbc36cb61ddcc0a18ade0ef5fb84f648e4884
Benny Nguyen <lamtruongnguyen888@gmail.com>
2025-09-29 20:09:06 -0700

    added files so they would appear in github

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamfiles/Auto/Team2Auto.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.teamfiles.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+public class Team2Auto extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        // init stuff goes here
+
+        while (opModeIsActive()) {
+            // do something or other
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamfiles/TeleOp/Team2TeleOp.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.teamfiles.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Team2TeleOp extends OpMode {
+    @Override
+    public void init() {
+        // do something here
+
+    }
+    @Override
+    public void loop() {
+        // do something here
+    }
+}
