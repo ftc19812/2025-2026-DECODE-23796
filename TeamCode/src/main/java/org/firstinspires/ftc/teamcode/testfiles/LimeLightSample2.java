ce36d6e11fbbb059851db2654559f9a389552f5b
Benny Nguyen <lamtruongnguyen888@gmail.com>
2025-10-07 19:53:17 -0700

    roadrunner sample edited

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testfiles/LimeLightSample2.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.testfiles;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+@TeleOp(name="Limelight Test 2")
+public class LimeLightSample2 extends LinearOpMode {
+
+    private Limelight3A limelight;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(0);
+
+        /*
+         * Starts polling for data.
+         */
+        limelight.start();
+        waitForStart();
+        while (opModeIsActive()) {
+            LLResult result = limelight.getLatestResult();
+            if (result != null) {
+                if (result.isValid()) {
+                    Pose3D botpose = result.getBotpose();
+                    telemetry.addData("tx", result.getTx());
+                    telemetry.addData("ty", result.getTy());
+                    telemetry.addData("Botpose", botpose.toString());
+                }
+            }
+        }
+    }
+}
